a30387af5da35f17b6255fbbcd15e61f46ed0c1d
dee-diaz <diazthediablo@yahoo.com>
2025-09-16 17:51:32 -0300

    Create basic Task shape

--- src/domain/Task.js
+++ src/domain/Task.js
@@ -1,1 +1,15 @@
 // Task model, basic task operations
+class Task {
+  constructor(title, description, scheduleDate, deadlineDate, priority) {
+    this._id = crypto.randomUUID();
+    this.title = title;
+    this.description = description;
+    this.scheduleDate = scheduleDate;
+    this.deadlineDate = deadlineDate;
+    this.priority = priority;
+  }
+}
+
+
+
+export default Task;
