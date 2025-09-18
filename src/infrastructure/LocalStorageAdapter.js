33edad76f83d096330570dd4b7694922915ac82e
dee-diaz <diazthediablo@yahoo.com>
2025-09-18 08:46:10 -0300

    Create local storage adapter

--- src/infrastructure/LocalStorageAdapter.js
+++ src/infrastructure/LocalStorageAdapter.js
@@ -1,1 +1,33 @@
 // localStorage implementation
+
+import StorageInterface from "./StorageInterface";
+
+class LocalStorageAdapter extends StorageInterface {
+  async get(key) {
+    try {
+      const data = localStorage.getItem(key);
+      return data ? JSON.parse(data) : null;
+    } catch (error) {
+      console.error("Error getting from localStorage", error);
+      return null;
+    }
+  }
+
+  async save(key, value) {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error("Error saving to localStorage", error);
+    }
+  }
+
+  async remove(key) {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error("Error removing from localStorage", error);
+    }
+  }
+}
+
+export default LocalStorageAdapter;
