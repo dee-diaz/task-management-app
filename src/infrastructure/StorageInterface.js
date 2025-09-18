153477c26eba8753d54be6c0776f4aa6e7e1c29a
dee-diaz <diazthediablo@yahoo.com>
2025-09-18 07:27:05 -0300

    Create an abstract interface for storage adapters

--- src/infrastructure/StorageInterface.js
+++ src/infrastructure/StorageInterface.js
@@ -1,1 +1,23 @@
-// Abstract storage contract
+/**
+ * An abstract interface for storage adapters
+ */
+
+class StorageInterface {
+  async save(key, value) {
+    throw new Error('Must implement');
+  }
+
+  async delete(key) {
+    throw new Error('Must implement');
+  }
+
+  async get(key) {
+    throw new Error('Must implement');
+  }
+
+  async clear() {
+    throw new Error('Must implement');
+  }
+}
+
+export default StorageInterface;
