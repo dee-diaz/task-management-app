34f9009b62b61298a04664885f40c75f2defe0ee
dee-diaz <diazthediablo@yahoo.com>
2025-09-30 15:19:46 -0300

    Render list title and today's date in the main section

--- src/presentation/renderers/TaskRenderer.js
+++ src/presentation/renderers/TaskRenderer.js
@@ -1,24 +1,35 @@
+import { DEFAULT_LISTS } from "../../utils/Constants";
+import { format } from "date-fns";
+
 class TaskRenderer {
   constructor(container) {
     this.container = container;
   }
 
   renderListTitle(listId) {
+    const main = document.querySelector(".main");
     const h1 = document.querySelector("#list-title");
     h1.textContent = listId;
+
+    if (listId === DEFAULT_LISTS.TODAY.id) {
+      const today = format(new Date(), "EEEE, MMMM d");
+      const para = document.createElement("p");
+      para.setAttribute("id", "todays-date");
+      para.textContent = today;
+      main.appendChild(para);
+    }
   }
 
   init() {
     const main = document.createElement("div");
     main.className = "main";
-    
+
     const h1 = document.createElement("h1");
     h1.setAttribute("id", "list-title");
     main.appendChild(h1);
-    
+
     this.container.appendChild(main);
   }
 }
-
 
 export default TaskRenderer;
