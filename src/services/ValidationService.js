724df8dda4ea2393c847d94b1759f8075e51a64f
dee-diaz <diazthediablo@yahoo.com>
2025-10-13 08:03:36 -0300

    Set focus on the title input if the input isn't validated

--- src/services/ValidationService.js
+++ src/services/ValidationService.js
@@ -4,6 +4,7 @@ class ValidationService {
     const minLength = 1;
     if (input.trim().length < minLength) {
       console.warn('Title should be at least 1 character');
+      document.querySelector("#task-title").focus();
       return false;
     } else {
       return true;
