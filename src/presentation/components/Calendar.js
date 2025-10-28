1a9daa409c3041f67998234e085bced56251144c
dee-diaz <diazthediablo@yahoo.com>
2025-10-28 10:37:43 -0300

    feat: allow past dates selection in date pickers

--- src/presentation/components/Calendar.js
+++ src/presentation/components/Calendar.js
@@ -8,30 +8,7 @@ function initDatePickers() {
     locale: localeEn,
     container: document.querySelector('#modal-task'),
     classes: 'custom-calendar-theme',
-    // minDate: new Date(),
     dateFormat: 'dd/MM/yyyy',
-    // onSelect({ date, datepicker }) {
-    //   if (!date) return;
-
-    //   const today = new Date();
-    //   const tomorrow = new Date();
-    //   tomorrow.setDate(today.getDate() + 1);
-
-    //   const normalize = (d) =>
-    //     new Date(d.getFullYear(), d.getMonth(), d.getDate());
-
-    //   const sel = normalize(date);
-    //   const t = normalize(today);
-    //   const tm = normalize(tomorrow);
-
-    //   if (sel.getTime() === t.getTime()) {
-    //     datepicker.$el.value = 'Today';
-    //   } else if (sel.getTime() === tm.getTime()) {
-    //     datepicker.$el.value = 'Tomorrow';
-    //   } else {
-    //     datepicker.$el.value = datepicker.formatDate(sel, 'dd/MM/yyyy');
-    //   }
-    // },
   };
 
   ['#task-schedule', '#task-deadline'].forEach((selector) => {
